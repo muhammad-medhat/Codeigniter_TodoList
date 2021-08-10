61604f9d96f29ee63c2dce8ab7c3398cd90aec95
Muhammad Medhat <muhammad.medhat@gmail.com>
2021-08-10 18:29:23 +0200

    css: styles

--- app/Views/todo_list.php
+++ app/Views/todo_list.php
@@ -10,14 +10,20 @@
         <?php foreach($todos as $t){ ?>
             <li class="list-group-item">
                 
-            <div>
-                
-                <input type="checkbox" <?= ($t->done == 0)? '': 'checked' ?> />
-                <span><?= $t->id?></span>
-                <span><?= $t->name?></span>
-                <span><?= $t->description?></span>
-                                
-            </div>    
+            
+                <div class="todo-row d-flex">
+                    <div class="data col">
+                        <input type="checkbox" <?= ($t->done == 0)? '': 'checked' ?> />
+                        <span><?= $t->id?></span>
+                        <span><?= $t->name?></span>
+                        <span><?= $t->description?></span>                        
+                    </div>
+                    <div class="ops col">
+                        <span><?= anchor(site_url("todos/update/$t->id"), 'Update...')?></span>
+                        <span><?= anchor(site_url("todos/Delete/$t->id"), 'Delete')?></span>
+                    </div>
+                    
+                </div>
 
             </li>
         <?php } ?>
